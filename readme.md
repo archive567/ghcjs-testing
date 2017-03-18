140dba0b1ab8fea7fd66c3bf78cbfc71d2be9473
tonyday567 <tonyday567@gmail.com>
2017-03-18 11:22:23 +1000

    added Vanilla, and cleanup of readme.lhs

--- readme.md
+++ readme.md
@@ -1,30 +1,17 @@
-<!doctype html>
-<html lang="en">
 <meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title>
-Haskell • GHCJS • Testing
-</title>
 <link rel="stylesheet" href="https://tonyday567.github.io/other/lhs.css">
-
-[ghcjs-testing](https://github.com/tonyday567/ghcjs-testing) [![Build Status](https://travis-ci.org/tonyday567/ghcjs-testing.png)](https://travis-ci.org/tonyday567/ghcjs-testing)
-==================================================================================================================================================================================
+<script type="text/javascript" async
+  src="https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-MML-AM_CHTML">
+</script>
+[ghcjs-testing](https://tonyday567.github.com/ghcjs-testing)
+============================================================
 
 Minimal ghcjs install and test.
 
-This is old-school web page development: grab some html page, and create
-some javascript that tweaks said page.
+This is old-school web page development: `readme.lhs` is used to render
+a html page, and the code below creates some javascript that tweaks said
+page.
 
-testing area
-------------
-
-<h2>
-test button
-</h2>
-<button class="click-me">
-Click me!
-</button>
-<!-- GHCJS scripts. -->
 <script language="javascript" src="other/ghcjs-testing.js"></script>
 code
 ====
@@ -32,30 +19,21 @@ code
 ``` {.sourceCode .literate .haskell}
 {-# LANGUAGE OverloadedStrings #-}
 import Protolude
+import Vanilla
+
+import GHCJS.Types
+import GHCJS.Marshal
+import GHCJS.Foreign
 import GHCJS.Foreign.Callback
-import Data.JSString -- This includes an IsString instance for JSString
-import GHCJS.Types (JSVal)
-import GHCJS.DOM (currentWindow)
-
-foreign import javascript unsafe
-  "console.log($1)" consoleLog :: JSString -> IO ()
-foreign import javascript unsafe
-  "alert($1)" alert :: JSString -> IO ()
-
-foreign import javascript unsafe "window.onload = $1"
-  onload :: Callback (IO ()) -> IO ()
+import GHCJS.Nullable (maybeToNullable, nullableToMaybe, Nullable)
+import Data.JSString.Text (textToJSString, textFromJSString, lazyTextToJSString, lazyTextFromJSString)
 
 main :: IO ()
 main = do
-  putStrLn ("a putStrLn" :: Text)
-  -- consoleLog $ ("a consoleLog")
-
-  w <- currentWindow
-  case w of
-     Nothing -> putStrLn ("no window in currentWindow" :: Text)
-     Just w' -> alert "an alert"
-
-  -- onload =<< asyncCallback (alert "post window.onload alert!!")
+    putStrLn ("putStrLn's go to the console.log" :: Text)
+    onload $ do
+       alert "window has loaded"
+       consoleLog "consolLog is a synonym for putStrLn"
 ```
 
 compiling
@@ -67,119 +45,18 @@ re-compile:
 -   stack build
 -   rendering of the page
 -   optimize (and copy) the js via
-    [closure](http://dl.google.com/closure-compiler/compiler-latest.zip)[^1]
+    [closure](http://dl.google.com/closure-compiler)
 
 <pre>
   <code style="white-space: pre-wrap;">
-stack build --exec "pandoc -f markdown+lhs -i readme.lhs -t html -o index.html" --exec "pandoc -f markdown+lhs -i readme.lhs -t markdown -o readme.md" --exec "java -jar $(stack path --local-bin)/closure-compiler-v20170124.jar --js_output_file=other/ghcjs-testing.js $(stack path --local-install-root)/bin/readme.jsexe/all.js"
+stack build --exec "pandoc -f markdown+lhs -i readme.lhs -t html -o index.html" --exec "pandoc -f markdown+lhs -i readme.lhs -t markdown -o readme.md" --exec "java -jar $(stack path --local-bin)/closure-compiler-v20170124.jar --js_output_file=other/ghcjs-testing.js $(stack path --local-install-root)/bin/ghcjs-testing.jsexe/all.js"
   </code>
 </pre>
-notes
-=====
+reference
+=========
 
-initial setup
--------------
+[ghcjs user guide](https://github.com/ghcjs/ghcjs/wiki/GHCJS-User-Guide)
 
-The repo was constructed using the following steps:
+[ghcjs-base](https://github.com/ghcjs/ghcjs-base)
 
--   `stack new ghcjs-testing readme-lhs`
--   edited stack.yaml to grab the `ghc-8.0.1` ghcjs documented
-    [here](https://docs.haskellstack.org/en/stable/ghcjs/).
--   `stack build`
-
-found ghcjs examples
---------------------
-
-https://github.com/luite/hs15-talk/blob/master/src/Common.hs
-
-[auto](https://github.com/mstksg/auto-examples/blob/master/src/TodoJS.hs)
-
-from [ghcjs-base](https://github.com/ghcjs/ghcjs-base):
-
-    import GHCJS.Foreign.Callback
-    import Data.JSString -- This includes an IsString instance for JSString
-    import GHCJS.Types (JSVal)
-
-    foreign import javascript unsafe
-      "require('console').log($1)" js_consoleLog :: JSVal -> IO ()
-
-    foreign import javascript unsafe
-      "require('fs').stat($1, $2)"
-      js_fsStat :: JSString -> Callback (JSVal -> JSVal -> IO ()) -> IO ()
-
-    main :: IO ()
-    main = do
-      cb <- asyncCallback2 $ \err stat -> js_consoleLog stat
-      js_fsStat "/home" cb
-      releaseCallback cb
-
-from [ghcjs-dom-hello](https://github.com/ghcjs/ghcjs-dom-hello)
-
-    main = do
-      putStrLn "<a href=\"http://localhost:3708/\">http://localhost:3708/</a>"
-      run 3708 $ do
-        Just doc <- currentDocument
-        body <- getBodyUnsafe doc
-        setInnerHTML body (Just "<h1>Kia ora (Hi)</h1>")
-        on doc D.click $ do
-            (x, y) <- mouseClientXY
-            newParagraph <- createElementUnsafe doc (Just "p") >>= unsafeCastTo HTMLParagraphElement
-            text <- createTextNode doc $ "Click " ++ show (x, y)
-            appendChild newParagraph text
-            appendChild body (Just newParagraph)
-            return ()
-
-        -- Make an exit button
-        exitMVar <- liftIO newEmptyMVar
-        exit <- createElementUnsafe doc (Just "span") >>= unsafeCastTo HTMLSpanElement
-        text <- createTextNode doc "Click here to exit"
-        appendChild exit text
-        appendChild body (Just exit)
-        on exit E.click $ liftIO $ putMVar exitMVar ()
-
-        -- Force all all the lazy evaluation to be executed
-        syncPoint
-
-        -- In GHC compiled version the WebSocket connection will end when this
-        -- thread ends.  So we will wait until the user clicks exit.
-        liftIO $ takeMVar exitMVar
-        setInnerHTML body (Just "<h1>Ka kite ano (See you later)</h1>")
-        return ()
-
-component testing (todo)
-========================
-
-react-flux
-----------
-
-http://blog.wuzzeb.org/full-stack-web-haskell/client.html
-
-https://facebook.github.io/flux/docs/in-depth-overview.html\#content
-
-http://hackage.haskell.org/package/react-flux
-
-https://bitbucket.org/wuzzeb/react-flux/src/tip/example/
-
-Material UI
------------
-
-http://www.material-ui.com/\#/
-
-footnotes
----------
-
-    <script language="javascript" src="other/rts.js"></script>
-    <script language="javascript" src="other/lib.js"></script>
-    <script language="javascript" src="other/out.js"></script>
-    <script language="javascript" src="other/runmain.js"></script>
-    <script language="javascript" src="other/all.js"></script>
-
-This doesn't work once you assume a browser window.
-
-<pre>
-  <code style="white-space: pre-wrap;">
-stack build --exec "node $(stack path --local-install-root)/bin/readme.jsexe/all.js"
-  </code>
-</pre>
-
-[^1]: from http://dl.google.com/closure-compiler/compiler-latest.zip
+[mdn](https://developer.mozilla.org/en-US/docs)
---
+++ src/Vanilla.hs
@@ -0,0 +1,278 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS_GHC -fno-warn-type-defaults #-}
+
+module Vanilla
+  ( Element(..)
+  , Selector
+  , consoleLog
+  , alert
+  , onload
+  , addClass
+  , removeClass
+  , setHtml
+  , docElement
+  , elementsOf
+  , elementOf
+  , getValue
+  , setValue
+  , itemId
+  , focus
+  , checked
+  , keyCode
+  , getHash
+  , on
+  , onWindow
+  , delegate
+  , JSVal
+  , toJSVal
+  , asyncCallback
+  ) where
+
+import Clay.Render (renderSelector)
+import Clay.Selector (Selector)
+import Data.Char
+import Data.Foldable
+import Data.Maybe (fromJust)
+import Data.Typeable
+import Lucid
+import Protolude hiding (Selector, on)
+import Unsafe.Coerce (unsafeCoerce)
+
+import qualified Data.Text.Lazy as LText
+import qualified Text.Read as Text
+
+import GHCJS.Types
+import GHCJS.Marshal
+import GHCJS.Foreign
+import GHCJS.Foreign.Callback
+import GHCJS.Nullable (maybeToNullable, nullableToMaybe, Nullable)
+import Data.JSString.Text (textToJSString, textFromJSString, lazyTextToJSString, lazyTextFromJSString)
+
+data Element = Element JSVal
+
+fromHtml = lazyTextToJSString . renderText
+
+fromSel = lazyTextToJSString . renderSelector
+
+-- * the big 3
+-- | console.log
+foreign import javascript unsafe
+    "console.log($1)"
+    js_consoleLog :: JSString -> IO ()
+
+consoleLog :: Text -> IO ()
+consoleLog a = js_consoleLog (textToJSString a)
+
+foreign import javascript unsafe
+  "alert($1)" js_alert :: JSString -> IO ()
+
+alert :: Text -> IO ()
+alert t = js_alert (textToJSString t)
+
+foreign import javascript unsafe "window.onload = $1"
+  js_onload :: Callback (IO ()) -> IO ()
+
+onload :: IO () -> IO ()
+onload f = do
+    f_ <- asyncCallback f
+    js_onload f_
+
+
+-- * element manipulation
+foreign import javascript unsafe
+    "$1.classList.add($2)"
+    js_classListAdd :: JSVal -> JSString -> IO ()
+
+addClass :: Element -> Selector -> IO ()
+addClass (Element el) sel =
+    js_classListAdd el (fromSel sel)
+
+foreign import javascript unsafe
+    "$1.classList.remove($2)"
+    js_classListRemove :: JSVal -> JSString -> IO ()
+
+removeClass :: Element -> Selector -> IO ()
+removeClass (Element el) sel =
+    js_classListRemove el (fromSel sel)
+
+foreign import javascript unsafe
+    "$1.innerHTML = $2"
+    js_innerHtml :: JSVal -> JSString -> IO ()
+
+setHtml :: Element -> Html () -> IO ()
+setHtml (Element el) html =
+    js_innerHtml el (fromHtml html)
+
+foreign import javascript unsafe
+    "document.querySelector($1)"
+    js_docQuerySelector :: JSString -> IO JSVal
+
+-- | element selection
+docElement :: Selector -> IO Element
+docElement sel =
+    Element <$> js_docQuerySelector (fromSel sel)
+
+foreign import javascript unsafe
+    "$1.querySelectorAll($2)"
+    js_querySelectorAll :: JSVal -> JSString -> IO JSVal
+
+elementsOf :: Element -> Selector -> IO JSVal
+elementsOf (Element el) sel = js_querySelectorAll el (fromSel sel)
+
+foreign import javascript unsafe
+    "$1.querySelector($2)"
+    js_querySelector :: JSVal -> JSString -> IO JSVal
+
+elementOf :: Element -> Selector -> IO JSVal
+elementOf (Element el) sel = js_querySelector el (fromSel sel)
+
+foreign import javascript unsafe
+    "$1.value"
+    js_getValue :: JSVal -> IO JSString
+
+getValue :: Element -> IO Text
+getValue (Element el) = textFromJSString <$> js_getValue el
+
+foreign import javascript unsafe
+    "$1.value = $2"
+    js_setValue :: JSVal -> JSString -> IO ()
+
+setValue :: Element -> Text -> IO ()
+setValue (Element el) val = js_setValue el (textToJSString val)
+
+foreign import javascript unsafe
+    "$1.dataset.id"
+    js_getDatasetId :: JSVal -> IO JSString
+
+itemId :: Element -> IO (Maybe Int)
+itemId (Element el) = do
+    i <- js_getDatasetId el
+    pure $ readMaybe (LText.unpack $ lazyTextFromJSString i)
+
+foreign import javascript unsafe
+    "$1.focus()"
+    js_focus :: JSVal -> IO ()
+
+focus :: Element -> IO ()
+focus (Element el) = js_focus el
+
+foreign import javascript unsafe
+    "$1.item($2)"
+    js_getItem :: JSVal -> Int -> IO (Nullable JSVal)
+
+-- getItem :: NodeList -> Int -> IO (Maybe Node)
+getItem :: JSVal -> Int -> IO (Maybe JSVal)
+getItem list n =
+  nullableToMaybe <$> js_getItem list n
+
+foreign import javascript unsafe
+    "$1.keyCode"
+    js_keyCode :: JSVal -> IO Int
+
+keyCode :: JSVal -> IO Int
+keyCode ev = js_keyCode ev
+
+foreign import javascript unsafe
+    "document.location.hash"
+    js_getHash :: IO JSString
+
+getHash :: IO JSString
+getHash = js_getHash
+
+foreign import javascript unsafe
+    "$1.checked"
+    js_checked :: JSVal -> IO Bool
+
+checked :: Element -> IO Bool
+checked (Element el) = js_checked el
+
+-- * event listening
+foreign import javascript unsafe
+    "$1.addEventListener($2,$3,$4)"
+    js_addEventListener :: JSVal -> JSString -> Callback (JSVal -> IO ()) -> Bool -> IO ()
+
+on :: Element -> Text -> (Element -> JSVal -> IO ()) -> IO () 
+on (Element el) uiAction handler = do
+  handler' <- asyncCallback1 (handler (Element el))
+  js_addEventListener el (textToJSString uiAction) handler' False
+
+foreign import javascript unsafe
+    "window.addEventListener($1,$2,$3)"
+    jsWindowAddEventListener :: JSString -> Callback (JSVal -> IO ()) -> Bool -> IO ()
+
+-- null = maybeToNullable Nothing
+
+onWindow :: LText.Text -> (JSVal -> IO ()) -> IO ()
+onWindow uiAction handler = do
+  handler' <- asyncCallback1 handler
+  jsWindowAddEventListener
+    (lazyTextToJSString uiAction)
+    handler'
+    False
+
+delegate :: Element -> Selector -> Text -> (Element -> JSVal -> IO ()) -> IO ()
+delegate (Element base) pattern0 ev action = do
+  let useCapture = ev == "blur" || ev == "focus"
+  dispatch <- asyncCallback1 $ applyIn (Element base) pattern0 action 
+  js_addEventListener base (textToJSString ev) dispatch useCapture
+
+foreign import javascript unsafe
+    "$1.target"
+    js_target :: JSVal -> IO JSVal
+
+foreign import javascript unsafe
+    "$1===$2"
+    js_eq :: JSVal -> JSVal -> IO Bool
+
+foreign import javascript unsafe
+    "$1.length"
+    js_length :: JSVal -> IO Int
+
+applyIn :: Element -> Selector -> (Element -> JSVal -> IO ()) -> JSVal -> IO ()
+applyIn base pattern0 action ev = do
+  t <- js_target ev
+  p <- elementsOf base pattern0
+  n <- js_length p
+  elem' <- findM
+           (\x -> do
+               item <- getItem p x
+               case item of
+                 Nothing -> pure False
+                 Just item' -> js_eq t item') [0..n]
+  case elem' of
+    Nothing -> pure ()
+    Just iElem -> do
+      eitem <- getItem p iElem
+      case eitem of
+        Nothing -> pure ()
+        Just eitem' -> do
+            li <- findUp (Element eitem') "li"
+            case li of
+              Nothing -> print "no li parent???"
+              Just li' -> action li' ev
+
+foreign import javascript unsafe
+    "$1.parentElement"
+    js_parentElement :: JSVal -> IO (Nullable JSVal)
+
+foreign import javascript unsafe
+    "$1.tagName"
+    js_tagName :: JSVal -> IO JSString
+
+-- | go up the tree and find an element, if it exists
+findUp :: Element -> LText.Text -> IO (Maybe Element)
+findUp (Element base) sel = do
+  parent <- nullableToMaybe <$> js_parentElement base
+  case parent of
+    Nothing -> pure Nothing
+    Just parent' -> do
+      name <- js_tagName parent'
+      if (LText.toLower $ lazyTextFromJSString name) == (LText.toLower sel)
+        then pure $ Just (Element parent')
+        else findUp (Element parent') sel
+
+findM :: (Monad m, Traversable t) => (a -> m Bool) -> t a -> m (Maybe a)
+findM p = fmap (getFirst . fold) . mapM 
+       (fmap First . (\x -> do
+           p' <- p x
+           pure $ if p' then Just x else Nothing))
